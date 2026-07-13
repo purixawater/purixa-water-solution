99c339e9fa9dcec586b9566fd8494843af807b1a
purixawater <arvindmbbokhani8741@gmail.com>
2026-07-14 00:56:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,15 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+const images = [
+  "slide1.jpg",
+  "slide2.jpg",
+  "slide3.jpg"
+];
+
+let i = 0;
+
+setInterval(() => {
+    i = (i + 1) % images.length;
+    document.getElementById("slide").src = images[i];
+}, 3000);
