42dcf1747e32d303605e6bc8b8d7ed344ed6bf36
purixawater <arvindmbbokhani8741@gmail.com>
2026-09-08 09:18:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "purixa-crm-pwa-v20.6";
+const CACHE_NAME = "purixa-crm-pwa-v20.8";
 const APP_SHELL = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
