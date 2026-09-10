932c7a8f090a1170fa440f7d09522727e06f9a67
purixawater <arvindmbbokhani8741@gmail.com>
2026-09-11 00:10:24 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='purixa-crm-pwa-v22.7';
-const APP_SHELL=['./','./index.html','./manifest.json','./purixa-logo.png','./icon-192.png','./icon-512.png'];
+const CACHE='purixa-crm-pwa-v22.8';
+const APP_SHELL=['./','./index.html','./manifest.json','./purixa-logo.png','./icon-192.png','./icon-512.png','./authorized-signature-clean.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));});
